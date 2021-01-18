7768f00fd43ff32dc1f825a3923784a8378c89f7
EhsanTgv <EhsanTgv@gmail.com>
2021-01-18 17:48:03 +0330

    Make the app crash for Log

--- app/src/main/java/com/taghavi/crashlytics/MainActivity.kt
+++ app/src/main/java/com/taghavi/crashlytics/MainActivity.kt
@@ -7,5 +7,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val text = "asfdf"
+        val number = text.toInt()
     }
 }
